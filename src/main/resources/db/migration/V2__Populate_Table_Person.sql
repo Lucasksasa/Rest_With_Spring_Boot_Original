b4c63dc3525c335e7bb1402ab2a64772414f016a
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-01-14 15:31:50 -0300

    Criada as versions das migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`address`, `first_name`, `gender`, `last_name`) VALUES
+('São Paulo', 'Ayrton', 'Male', 'Senna'),
+('Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+('Porbandar - India', 'Indira', 'Male', 'Gandhi'),
+('Porbandar - India', 'Mahatma', 'Female', 'Gandhi'),
+('Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+('Mvezo - South Africa', 'Nelson', 'Male', 'Mvezo'),
+('Smiljan - Croácia', 'Nikola', 'Male', 'Tesla');
